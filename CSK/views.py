1efc9d036acc686eba4fe1759f0b49f256f3f10a
keerthi1 <eerthi123@gmail.com>
2024-11-25 19:38:05 +0530

    first commit

---
+++ CSK/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def ruturaj(request):
+    return HttpResponse('he is the captain of csk')
---
+++ MI/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def hardik(request):
+    return HttpResponse("he is the captain of mi")
